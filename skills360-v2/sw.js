bf054bf1b253d7e1655c15ca1581c84813314c52
nourabobaker-create <noura.bobaker@tu.edu.ly>
2026-09-22 01:14:59 +0200

    Refresh V2 offline cache for final tested assets

--- skills360-v2/sw.js
+++ skills360-v2/sw.js
@@ -1,4 +1,4 @@
-const CACHE='tobruk-skills360-v2-1';
+const CACHE='tobruk-skills360-v2-2';
 const ASSETS=['./','./index.html','./style.css','./app.mjs','./scene.mjs','./engine.mjs','./content.mjs','./source.json','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('tobruk-skills360-v2-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
